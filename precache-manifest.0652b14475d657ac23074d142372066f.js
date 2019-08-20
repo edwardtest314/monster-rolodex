154e6542d4fd946e2971c37b730fbe327454a0f5
Sunny Chan <edward1879314@gmail.com>
2019-08-20 15:07:18 +0800

    Updates

---
+++ precache-manifest.0652b14475d657ac23074d142372066f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab198d9866b8ccb0cc7287fb66a18fa6",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "a3acae4d56c5c6a4ccb6",
+    "url": "/monster-rolodex/static/css/main.4f14873c.chunk.css"
+  },
+  {
+    "revision": "32061f2f4fe1438d5314",
+    "url": "/monster-rolodex/static/js/2.67be95f2.chunk.js"
+  },
+  {
+    "revision": "a3acae4d56c5c6a4ccb6",
+    "url": "/monster-rolodex/static/js/main.fe089b95.chunk.js"
+  },
+  {
+    "revision": "58aab954714b377e1cb3",
+    "url": "/monster-rolodex/static/js/runtime~main.62800703.js"
+  }
+]);
